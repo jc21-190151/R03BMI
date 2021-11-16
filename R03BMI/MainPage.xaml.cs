5b94178e9d1120b572064da2a1509fadd4d620b4
友督 三浦 <190151@jc-21.jp>
2021-11-16 12:30:47 +0900

    BMI計算処理完成

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -18,7 +18,39 @@ namespace R03BMI
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-
+            try
+            {
+                double h = double.Parse(heighit.Text);
+                double w = double.Parse(weighit.Text);
+                string sin="m";
+                string tai="kg";
+                //身長がｃｍで入力されたとき
+                if (h > 3)
+                {
+                    h = h / 100;
+                    sin="cm";
+                }
+                //体重がｇで入力されたと
+                if (w > 300)
+                {
+                    w = w / 1000;
+                    tai="g";
+                }
+                //BMI導出
+                double b = w / h / h;
+                //桁落ち防止処理
+                b = b * 10;
+                //BMI四捨五入
+                double bmi = Math.Round(b);
+                //正常な数値へ戻す処理
+                bmi /= 10;
+                //printf("身長%5.2f m、体重%6.2f kgの人のBMI = %6.2f\n", height, weight, bmi);
+                result.Text = "身長" + sin + h + "体重" + tai + w + "の人のBMIは" + bmi + "です。";
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
