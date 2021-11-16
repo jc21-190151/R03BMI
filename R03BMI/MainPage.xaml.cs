5af3c874e2d5da7f7e81ae4b6160eb8bc31dd972
友督 三浦 <190151@jc-21.jp>
2021-11-16 15:59:15 +0900

    fix 表示について

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -22,25 +22,44 @@ namespace R03BMI
             {
                 double h = double.Parse(heighit.Text);
                 double w = double.Parse(weighit.Text);
+                string sin="m";
+                string tai="kg";
+                int s=0;
+                int t=0;
                 string him ="";
                 //身長がｃｍで入力されたとき
                 if (h > 3)
                 {
                     h = h / 100;
+                    sin = "cm";
+                    s=1;
                 }
                 //体重がｇで入力されたと
                 if (w > 300)
                 {
                     w = w / 1000;
+                    tai = "g";
+                    t=1;
                 }
                 //BMI導出
                 double b = w / h / h;
+                //身長をの単位を治す
+                if (s > 0)
+                {
+                    h=h*100;
+                }
+                //体重の単位をそろえる
+                if (t > 0)
+                {
+                    w=w*1000;
+                }
+                
                 //桁落ち防止処理
-                b = b * 10;
+                //b = b * 10;
                 //BMI四捨五入
                 double bmi = Math.Round(b);
                 //正常な数値へ戻す処理
-                bmi /= 10;
+                //bmi /= 10;
                 //肥満度計算
                 if (bmi < 18.50)
                 {
@@ -62,7 +81,7 @@ namespace R03BMI
                     him = "肥満 (4度)";
                 }
                 //printf("身長%5.2f m、体重%6.2f kgの人のBMI = %6.2f\n", height, weight, bmi);
-                result.Text = "身長" + h + "m 体重"  + w + "kg の人のBMIは" + bmi + "肥満度は" + him + "です。";
+                result.Text = "身長" + h +sin+ "体重"  + w +tai+ "の人のBMIは" + bmi + "肥満度は" + him + "です。";
             }
             catch (FormatException ex)
             {
